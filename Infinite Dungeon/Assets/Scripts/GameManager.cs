d57e5fe199fd5a3c1fa9e984ba769d30678453ab
GabrielBCarvalho <gabrielmattheus.carvalho@gmail.com>
2017-10-31 15:44:29 -0200

    Movement fixed, action menu during battle added

---
+++ Infinite Dungeon/Assets/Scripts/GameManager.cs
@@ -0,0 +1,44 @@
+﻿/* Universidade de Sao Paulo - ICMC - Outubro de 2017
+   Autores: Klinsmann Hengles, Felipe Torres e Gabriel Carvalho
+   Script desenvolvido para o jogo Infinite Dungeon
+   Descrição: Script que comanda as ações principais do jogo, como quando surgem os inimigos, os personagens, distribui a experiencia, etc.*/ 
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+
+	private Vector2 pointToGo; // Ponto ate onde o character vai se mover
+	public GameObject character; // Referencia ao character, o objeto que sera movido
+	public GameObject battleUI;
+
+	// Use this for initialization
+	void Start () {
+		battleUI.SetActive(false);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+		if (Input.GetMouseButtonDown(0)) {
+			RaycastHit hit;
+			Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+
+			// Verify if a character was selected BEFORE, and not exactly now. 
+			// We need to do it to obtain a good synchronization with the OnMouseDown() method from PlayerBehaviour. 
+			if ((character.GetComponent<PlayerBehaviour> ().State == PlayerBehaviour.STATE.SELECTED) && 
+				(!Physics2D.Raycast(new Vector2(Camera.main.ScreenToWorldPoint(Input.mousePosition).x, 
+					Camera.main.ScreenToWorldPoint(Input.mousePosition).y), Vector2.zero, 0f))) {
+				pointToGo = (Vector2)Camera.main.ScreenToWorldPoint (Input.mousePosition);
+				character.GetComponent<PlayerBehaviour> ().FinalPosition = pointToGo;
+				character.GetComponent<PlayerBehaviour> ().State = PlayerBehaviour.STATE.MOVING;
+				print ("Entrou");
+				battleUI.SetActive(false);
+			}
+		}
+
+	}
+		
+}
---
+++ Infinite Dungeon/Assets/Scripts/PlayerBehaviour.cs
@@ -0,0 +1,85 @@
+﻿/* Universidade de Sao Paulo - ICMC - Outubro de 2017
+   Autores: Klinsmann Hengles, Felipe Torres e Gabriel Carvalho
+   Script desenvolvido para o jogo Infinite Dungeon
+   Descrição: Script que descreve o comportamento do personagem*/
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerBehaviour : MonoBehaviour {
+
+	public enum STATE {NOTSELECTED, SELECTED, MOVING} // possiveis estados do personagem
+
+	public float teste;
+ 
+	public float speed; // velocidade da movimentacao do personagem
+	private Vector2 currentPosition; // posicao inicial do personagem
+	private Vector2 finalPosition; // posicao para a qual o personagem vai se mover
+	private STATE state; // determina o estado do personagem
+
+	public GameObject battleUI;			// GameObject that contains all UI Battle components
+	public GameObject attackButton;		
+	public GameObject specialButton;
+
+
+	public Vector2 FinalPosition {
+		get {
+			return finalPosition;
+		}
+		set { 
+			finalPosition = value; 
+		}
+	}
+
+	public STATE State {
+		get {
+			return state;
+		}
+		set { 
+			state = value; 
+		}
+	}
+
+	// Use this for initialization
+	void Start () {
+		currentPosition = gameObject.transform.position; // pega a posicao inicial do personagem
+		state = STATE.NOTSELECTED;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (state == STATE.MOVING) {
+			speed = 1f;
+			teste += Time.deltaTime / 1f;
+			transform.position = Vector2.Lerp (currentPosition, finalPosition, teste);
+		}
+
+	}
+
+	public void OnClickAttack(){
+		print ("Attack");
+		if (state == STATE.SELECTED) {
+
+		}
+	}
+
+	public void OnClickSpecial(){
+		print ("Special");
+		if (state == STATE.SELECTED) {
+
+		}
+	}
+
+	// Funcao que detecta a colisao entre um objeto com colisor e o mouse
+	void OnMouseDown() {
+		battleUI.transform.position = gameObject.transform.position;
+		battleUI.SetActive(true);
+		state = STATE.SELECTED;
+		print ("OnMouseDown");
+
+		teste = 0;
+		currentPosition = transform.position;
+	}
+
+}
